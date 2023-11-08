592ea7a5f48bf7e112c2e5fe1851e1f84cfc3318
Vishwa <88319493+vishwajitparasar-bsft@users.noreply.github.com>
2023-11-08 20:46:46 +0530

    Modified base_client.rb to return connection error when Apollo returns 5xx status codes

--- lib/apollo/clients/base_client.rb
+++ lib/apollo/clients/base_client.rb
@@ -4,8 +4,24 @@ module Apollo
 
   class ConnectionError < StandardError
   end
-
+  class BadRequest < RequestError; end                     #400
+  class NotFoundError < RequestError; end                  #404
+  class RequestTimeout < RequestError; end                 #408
+  class TooManyConnectionsFromThisIP < RequestError; end   #421
+  class TooManyRequests < RequestError; end                #429
   class BaseClient
+
+    ERROR_MAP = {
+      400 => BadRequest,
+      404 => NotFoundError,
+      408 => RequestTimeout,
+      421 => TooManyConnectionsFromThisIP,
+      429 => TooManyRequests,
+      502 => ConnectionError,
+      503 => ConnectionError,
+      504 => ConnectionError
+    }
+    ERROR_MAP.default = RequestError
     def handle_request(url, http_verb, params: nil, body: nil)
       result = nil
       Apollo.connection.with do |faraday_connection|
@@ -41,12 +57,13 @@ module Apollo
       [body, result.headers]
     end
 
-    def handle_result(result)
-      if (result.status >= 200 && result.status < 300) || result.status == 404
+    def handle_results(result)
+      if result.status >= 200 && result.status < 300 || result.status == 404
         JSON.load(result.body)
       else
-        raise RequestError, "Apollo error status=#{result.status} #{result.body}"
+        raise ERROR_MAP[result.status], "Apollo error status=#{result.status} #{result.body}"
       end
     end
+
   end
 end
